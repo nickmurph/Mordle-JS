b78392172f38cd5627319cbfcbe7a39b9d523f95
nickmurph <nfmurphy@buffalo.edu>
2022-10-25 20:57:43 -0600

    browser issues sorted, notes updated

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
-// import wordList from '/data/wordList.json' assert {type: 'json'};
-//import assertions not supported in firefox but this works in other browsers
-//throws MIME error without the assertion
-
-//works in all browsers tested
 const response = await fetch('/data/wordList.json');
 const wordList = await response.json();
 
 
-console.log(wordList[0]);
+console.log(`the wordlist is ${wordList.length} words long, from ${wordList[0]} to ${wordList[wordList.length-1]}!`);
 
-//alternate method written when encountering issues above
-//pasted the entire json object into a js file as an array
-//wrote a getter function and imported it here
-// import {getWordList} from './wordList.js';
-// let wordList = getWordList();
 
-// console.log(wordList[0]);
